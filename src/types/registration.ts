03918c288eb1a678a20a3ab90502ade42d9c56a2
Skie-ZemblHeroku <salesforce+zemblheroku@skie.com.au>
2023-09-06 16:34:18 +0700

    [Update] - Fix Types

---
+++ src/types/registration.ts
@@ -0,0 +1,4 @@
+import { BOTH_VALUE, ELECTRICITY_VALUE, GAS_VALUE } from '../constants'
+
+export type EnergyType = GAS_VALUE | ELECTRICITY_VALUE | BOTH_VALUE
+export type PlanType = GAS_VALUE | ELECTRICITY_VALUE
