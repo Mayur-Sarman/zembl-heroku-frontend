9168bf7664b0dbef9221634d9bbd65fbfccb3123
Skie-ZemblHeroku <salesforce+zemblheroku@skie.com.au>
2023-09-12 18:04:51 +0700

    [Update] - Remove deprecated page

--- src/pages/ReviewPage/ReviewPage.tsx
+++ src/pages/ReviewPage/ReviewPage.tsx
@@ -1,76 +1,76 @@
-import PageWrapper from '../../components/PageWrapper'
-import RegistrationStep from '../../components/RegistrationStep'
-import { useContext } from 'react'
-import RegistrationContext from '../../contexts/RegistrationContext'
-import { useForm } from 'react-hook-form'
-import SelectedPlans from '../../components/SelectedPlans'
-import { Button, Typography } from '@material-tailwind/react'
-import { useNavigate } from 'react-router-dom'
-import { BOTH_VALUE } from '../../constants'
+// import PageWrapper from '../../components/PageWrapper'
+// import RegistrationStep from '../../components/RegistrationStep'
+// import { useContext } from 'react'
+// import RegistrationContext from '../../contexts/RegistrationContext'
+// import { useForm } from 'react-hook-form'
+// import SelectedPlans from '../../components/SelectedPlans'
+// import { Button, Typography } from '@material-tailwind/react'
+// import { useNavigate } from 'react-router-dom'
+// import { BOTH_VALUE } from '../../constants'
 
-const MOCKUP_HTML = `
-<p><b>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b> Sed condimentum pretium maximus. Maecenas vulputate sollicitudin sem, in sollicitudin ante laoreet eu. Vivamus quis ante laoreet, vestibulum nisi quis, ornare libero. Ut semper neque vel lectus venenatis, a pellentesque nisl imperdiet. Etiam luctus elementum quam vitae dictum. Etiam convallis dapibus felis, sed tempus velit blandit non. Morbi ornare nunc accumsan est mollis aliquet. Quisque sodales scelerisque lectus eu sagittis. Suspendisse blandit ante quis quam iaculis, in porta quam egestas. Nulla in suscipit est. Curabitur id tempor neque. Cras non mauris ultricies, posuere purus vel, aliquet ex. Duis molestie lacus ut arcu auctor aliquam ut nec eros.</p>
-<p><i>Nullam porta sem ac magna convallis condimentum. Ut eu augue nisi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</i> Nulla mattis risus sed sodales feugiat. Aliquam eu lorem venenatis, dignissim est nec, molestie risus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Morbi non malesuada metus. Nam porttitor a ante nec mattis. Aenean id felis in erat tempor volutpat.</p>
-<p>Morbi nunc mi, porta ut est ac, suscipit tempor mauris. <pre>Nullam sed blandit neque, vitae scelerisque mi. Ut ut consectetur libero.</pre> Sed vel leo pellentesque, tempus tortor eu, semper arcu. Nullam elementum sit amet justo sit amet pharetra. Nulla eu sem sem. Aliquam et vulputate lacus, id ultricies libero. Maecenas ullamcorper convallis erat, eleifend tristique justo aliquam sed.</p>
-<p><span style='color: red; font-size: 18px; font-weight: 700'>Integer eleifend pretium tristique.</span> Aenean euismod, ex quis commodo tempus, arcu tellus convallis ipsum, nec malesuada enim risus vulputate est. Praesent faucibus dignissim tellus et maximus. Vivamus viverra, velit quis maximus congue, neque lacus venenatis orci, vitae tristique lectus nisl vitae arcu. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; In ut risus venenatis, euismod nisl sit amet, dictum risus. Maecenas nisl urna, commodo rhoncus porttitor eget, tincidunt sed mauris. Quisque quis dui pharetra, convallis elit sit amet, faucibus leo. Ut convallis sit amet elit imperdiet bibendum. Vestibulum nec turpis est. Praesent in leo vitae purus vulputate ornare ac eget neque. Etiam in sodales risus, et fringilla purus. Donec condimentum a turpis sit amet porttitor.</p>`
+// const MOCKUP_HTML = `
+// <p><b>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b> Sed condimentum pretium maximus. Maecenas vulputate sollicitudin sem, in sollicitudin ante laoreet eu. Vivamus quis ante laoreet, vestibulum nisi quis, ornare libero. Ut semper neque vel lectus venenatis, a pellentesque nisl imperdiet. Etiam luctus elementum quam vitae dictum. Etiam convallis dapibus felis, sed tempus velit blandit non. Morbi ornare nunc accumsan est mollis aliquet. Quisque sodales scelerisque lectus eu sagittis. Suspendisse blandit ante quis quam iaculis, in porta quam egestas. Nulla in suscipit est. Curabitur id tempor neque. Cras non mauris ultricies, posuere purus vel, aliquet ex. Duis molestie lacus ut arcu auctor aliquam ut nec eros.</p>
+// <p><i>Nullam porta sem ac magna convallis condimentum. Ut eu augue nisi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</i> Nulla mattis risus sed sodales feugiat. Aliquam eu lorem venenatis, dignissim est nec, molestie risus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Morbi non malesuada metus. Nam porttitor a ante nec mattis. Aenean id felis in erat tempor volutpat.</p>
+// <p>Morbi nunc mi, porta ut est ac, suscipit tempor mauris. <pre>Nullam sed blandit neque, vitae scelerisque mi. Ut ut consectetur libero.</pre> Sed vel leo pellentesque, tempus tortor eu, semper arcu. Nullam elementum sit amet justo sit amet pharetra. Nulla eu sem sem. Aliquam et vulputate lacus, id ultricies libero. Maecenas ullamcorper convallis erat, eleifend tristique justo aliquam sed.</p>
+// <p><span style='color: red; font-size: 18px; font-weight: 700'>Integer eleifend pretium tristique.</span> Aenean euismod, ex quis commodo tempus, arcu tellus convallis ipsum, nec malesuada enim risus vulputate est. Praesent faucibus dignissim tellus et maximus. Vivamus viverra, velit quis maximus congue, neque lacus venenatis orci, vitae tristique lectus nisl vitae arcu. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; In ut risus venenatis, euismod nisl sit amet, dictum risus. Maecenas nisl urna, commodo rhoncus porttitor eget, tincidunt sed mauris. Quisque quis dui pharetra, convallis elit sit amet, faucibus leo. Ut convallis sit amet elit imperdiet bibendum. Vestibulum nec turpis est. Praesent in leo vitae purus vulputate ornare ac eget neque. Etiam in sodales risus, et fringilla purus. Donec condimentum a turpis sit amet porttitor.</p>`
 
-const MOCKUP_ELECTRIC_PLAN = {
-  brandIconSrc: 'https://logos-world.net/wp-content/uploads/2021/08/Among-Us-Logo.png',
-  planName: 'Among Us Red Corp',
-  termAndConditions: MOCKUP_HTML,
-}
+// const MOCKUP_ELECTRIC_PLAN = {
+//   brandIconSrc: 'https://logos-world.net/wp-content/uploads/2021/08/Among-Us-Logo.png',
+//   planName: 'Among Us Red Corp',
+//   termAndConditions: MOCKUP_HTML,
+// }
 
-const MOCKUP_GAS_PLAN = {
-  brandIconSrc: 'https://i.redd.it/wyqn5yu8x7o51.png',
-  planName: 'Among Us Blue Corp',
-  termAndConditions: MOCKUP_HTML,
-}
+// const MOCKUP_GAS_PLAN = {
+//   brandIconSrc: 'https://i.redd.it/wyqn5yu8x7o51.png',
+//   planName: 'Among Us Blue Corp',
+//   termAndConditions: MOCKUP_HTML,
+// }
 
-const ReviewPage = () => {
-  const navigate = useNavigate()
-  const { registrationData } = useContext(RegistrationContext)
+// const ReviewPage = () => {
+//   const navigate = useNavigate()
+//   const { registrationData } = useContext(RegistrationContext)
 
-  // On load page get data from context
-  const { handleSubmit, register, control } = useForm()
+//   // On load page get data from context
+//   const { handleSubmit, register, control } = useForm()
 
-  const onSubmit = (data: Record<string, string | string[]>) => {
-    console.log(data)
+//   const onSubmit = (data: Record<string, string | string[]>) => {
+//     console.log(data)
 
-    console.log(registrationData)
-    // Call API
-    // Put data to context
-    navigate('/rezembl-no-thank-you')
-  }
+//     console.log(registrationData)
+//     // Call API
+//     // Put data to context
+//     navigate('/rezembl-no-thank-you')
+//   }
 
-  return (
-    <PageWrapper>
-      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-6 w-full">
-        <div className="lg:h-32 lg:py-3 lg:px-14 lg:mt-6">
-          <div className="hidden lg:block">
-            <RegistrationStep currentStep={3} />
-          </div>
-        </div>
-        <hr className="hidden lg:block" />
-        <SelectedPlans
-          title="Your Plans"
-          onEditClick={() => console.log('Edit')}
-          register={register}
-          control={control}
-          gasPlan={MOCKUP_GAS_PLAN}
-          electricityPlan={MOCKUP_ELECTRIC_PLAN}
-          energyType={BOTH_VALUE}
-        />
+//   return (
+//     <PageWrapper>
+//       <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-6 w-full">
+//         <div className="lg:h-32 lg:py-3 lg:px-14 lg:mt-6">
+//           <div className="hidden lg:block">
+//             <RegistrationStep currentStep={3} />
+//           </div>
+//         </div>
+//         <hr className="hidden lg:block" />
+//         <SelectedPlans
+//           title="Your Plans"
+//           onEditClick={() => console.log('Edit')}
+//           register={register}
+//           control={control}
+//           gasPlan={MOCKUP_GAS_PLAN}
+//           electricityPlan={MOCKUP_ELECTRIC_PLAN}
+//           energyType={BOTH_VALUE}
+//         />
 
-        <Button type="submit" className="!zembl-btn">
-          Submit Application
-        </Button>
-        <Typography className="text-xs text-zembl-p">
-          Note: By submitting this application, you acknowledge that you have read and agree to the terms and conditions
-          of this offer.
-        </Typography>
-      </form>
-    </PageWrapper>
-  )
-}
+//         <Button type="submit" className="!zembl-btn">
+//           Submit Application
+//         </Button>
+//         <Typography className="text-xs text-zembl-p">
+//           Note: By submitting this application, you acknowledge that you have read and agree to the terms and conditions
+//           of this offer.
+//         </Typography>
+//       </form>
+//     </PageWrapper>
+//   )
+// }
 
-export default ReviewPage
+// export default ReviewPage
