4b7e384ee28e93ec14a2bd7116922b4cff878f0b
JamesTeerawat <james@skie.com.au>
2023-11-17 02:55:15 +0700

    basic 1

--- src/pages/BasicInfoPage/BasicInfoPage1/BasicInfoPage1.tsx
+++ src/pages/BasicInfoPage/BasicInfoPage1/BasicInfoPage1.tsx
@@ -1,18 +1,18 @@
-// import { FieldValues, useForm } from 'react-hook-form'
+import { FieldValues, useForm } from 'react-hook-form'
 
-// import { useRegistration } from '../../../hooks/useRegistration'
-// import { lazy, useEffect } from 'react'
-// import { REGISTRATION_TYPE_BUSINESS } from '../../../constants'
-// import { buildLeadPayload } from '../../../api/lead'
+import { useRegistration } from '../../../hooks/useRegistration'
+import { lazy, useEffect } from 'react'
+import { REGISTRATION_TYPE_BUSINESS } from '../../../constants'
+import { buildLeadPayload } from '../../../api/lead'
 
-// const BasicBusinessDetailsForm = lazy(() => import('../../../components/Forms/BasicInfos/BasicBusinessDetailsForm'))
-// const EnergyTypeForm = lazy(() => import('../../../components/Forms/BasicInfos/EnergyTypeForm'))
-// const EnergySpendForm = lazy(() => import('../../../components/Forms/BasicInfos/EnergySpendForm'))
-// const PageNavigationActions = lazy(() => import('../../../components/PageNavigationActions'))
+const BasicBusinessDetailsForm = lazy(() => import('../../../components/Forms/BasicInfos/BasicBusinessDetailsForm'))
+const EnergyTypeForm = lazy(() => import('../../../components/Forms/BasicInfos/EnergyTypeForm'))
+const EnergySpendForm = lazy(() => import('../../../components/Forms/BasicInfos/EnergySpendForm'))
+const PageNavigationActions = lazy(() => import('../../../components/PageNavigationActions'))
 
 const BasicInfoPage1 = () => {
   // On load page get data from context
-  /*const { updateLeadMutation, registrationData } = useRegistration()
+  const { updateLeadMutation, registrationData } = useRegistration()
   const { handleSubmit, control, watch, formState } = useForm({
     mode: 'all',
     defaultValues: registrationData as FieldValues,
@@ -39,19 +39,18 @@ const BasicInfoPage1 = () => {
     if (updateLeadMutation.isSuccess) {
       updateLeadMutation.reset()
     }
-  }, [updateLeadMutation])*/
+  }, [updateLeadMutation])
 
-  // const isNonBusiness = registrationData?.registrationType !== REGISTRATION_TYPE_BUSINESS
+  const isNonBusiness = registrationData?.registrationType !== REGISTRATION_TYPE_BUSINESS
 
   return (
-    // <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-6 w-full md:w-10/12">
-    //   <BasicBusinessDetailsForm control={control} hideNonBusiness={isNonBusiness} />
-    //   <EnergyTypeForm control={control} isMoving={isMoving as string} hideNonBusiness={isNonBusiness} />
-    //   {!isNonBusiness ? <EnergySpendForm control={control} billingType={billFrequency as string} /> : null}
+    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-6 w-full md:w-10/12">
+      <BasicBusinessDetailsForm control={control} hideNonBusiness={isNonBusiness} />
+      <EnergyTypeForm control={control} isMoving={isMoving as string} hideNonBusiness={isNonBusiness} />
+      {!isNonBusiness ? <EnergySpendForm control={control} billingType={billFrequency as string} /> : null}
 
-    //   <PageNavigationActions prevLink="/" nextDisabled={!formState.isValid} />
-    // </form>
-    <></>
+      <PageNavigationActions prevLink="/" nextDisabled={!formState.isValid} />
+    </form>
   )
 }
 
