53e24058013c4fb6574bc45580db6e624c26a9c5
JamesTeerawat <james@skie.com.au>
2023-11-17 03:07:04 +0700

    Revert "TEST"
    This reverts commit 2b237465798199e8ef9a44872ccfbe7d7f8494c5.

--- src/pages/BasicInfoPage/BasicInfoPageTitle.tsx
+++ src/pages/BasicInfoPage/BasicInfoPageTitle.tsx
@@ -1,44 +1,43 @@
-// import { CheckCircleIcon } from '@heroicons/react/20/solid'
+import { CheckCircleIcon } from '@heroicons/react/20/solid'
 // import { Typography } from '@material-tailwind/react'
-// import { PropsWithChildren, useContext} from 'react'
-// import RegistrationContext from '../../contexts/RegistrationContext'
-// import { REGISTRATION_TYPE_BUSINESS } from '../../constants'
+import { PropsWithChildren, useContext} from 'react'
+import RegistrationContext from '../../contexts/RegistrationContext'
+import { REGISTRATION_TYPE_BUSINESS } from '../../constants'
 
-// const CheckIcon = ({ children }: PropsWithChildren) => {
-//   return (
-//     <span className="flex gap-1 text-zembl-p items-center">
-//       <CheckCircleIcon fill="#8EFF95" viewBox="1 1 18 18" className="w-5 h-5 bg-zembl-p rounded-full" />
-//       <p className="text-xs lg:text-base">
-//         {children}
-//       </p>
-//     </span>
-//   )
-// }
+const CheckIcon = ({ children }: PropsWithChildren) => {
+  return (
+    <span className="flex gap-1 text-zembl-p items-center">
+      <CheckCircleIcon fill="#8EFF95" viewBox="1 1 18 18" className="w-5 h-5 bg-zembl-p rounded-full" />
+      <p className="text-xs lg:text-base">
+        {children}
+      </p>
+    </span>
+  )
+}
 
 const BasicInfoPageTitle = () => {
-  // const { registrationData } = useContext(RegistrationContext)
+  const { registrationData } = useContext(RegistrationContext)
   return (
-    // <div className="flex flex-col gap-y-3 items-center">
-    //   <p className="text-zembl-p text-3xl md:text-5xl font-bold">
-    //     Compare energy plans
-    //   </p>
-    //   <p className="text-zembl-p px-12 lg:text-base max-w-lg">
-    //     Please have at hand the following
-    //   </p>
-    //   <div className="flex flex-col gap-x-3 gap-y-1 md:flex-row">
-    //     <span className="flex gap-1 text-zembl-p items-center">
-    //       <CheckIcon>Latest Bill</CheckIcon>
-    //     </span>
-    //     <span className="flex gap-1 text-zembl-p items-center">
-    //       <CheckIcon>Drivers License, Passport or Medicare Card</CheckIcon>
-    //     </span>
-    //     {registrationData?.registrationType === REGISTRATION_TYPE_BUSINESS ? 
-    //     <span className="flex gap-1 text-zembl-p items-center">
-    //       <CheckIcon>ABN</CheckIcon>
-    //     </span> : null}
-    //   </div>
-    // </div>
-    <></>
+    <div className="flex flex-col gap-y-3 items-center">
+      <p className="text-zembl-p text-3xl md:text-5xl font-bold">
+        Compare energy plans
+      </p>
+      <p className="text-zembl-p px-12 lg:text-base max-w-lg">
+        Please have at hand the following
+      </p>
+      <div className="flex flex-col gap-x-3 gap-y-1 md:flex-row">
+        <span className="flex gap-1 text-zembl-p items-center">
+          <CheckIcon>Latest Bill</CheckIcon>
+        </span>
+        <span className="flex gap-1 text-zembl-p items-center">
+          <CheckIcon>Drivers License, Passport or Medicare Card</CheckIcon>
+        </span>
+        {registrationData?.registrationType === REGISTRATION_TYPE_BUSINESS ? 
+        <span className="flex gap-1 text-zembl-p items-center">
+          <CheckIcon>ABN</CheckIcon>
+        </span> : null}
+      </div>
+    </div>
   )
 }
 
