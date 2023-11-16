b825c25b8497b5c19f71d136c5f038a7668d131d
JamesTeerawat <james@skie.com.au>
2023-11-17 03:06:41 +0700

    Revert "update basic 2"
    This reverts commit a464719f8fc77e84c2e3235815661f8f3ee981ad.

--- src/pages/BasicInfoPage/BasicInfoPage2/BasicInfoPage2.tsx
+++ src/pages/BasicInfoPage/BasicInfoPage2/BasicInfoPage2.tsx
@@ -1,5 +1,4 @@
 import { FieldValues, useForm } from 'react-hook-form'
-import { Button } from '@material-tailwind/react'
 
 // import { useNavigate } from 'react-router-dom'
 // import { REQUIRED_VALIDATION } from '../../../constants/validation'
@@ -8,19 +7,19 @@ import { Button } from '@material-tailwind/react'
 // import { Site } from '../../../api/site'
 // import { convertPreference } from '../../../api/common'
 // import { ZEMBL_DEBUG_MODE } from '../../../constants/misc'
-// import { lazy } from 'react'
+import { lazy } from 'react'
 
 // const RegistrationStep = lazy(() => import('../../../components/RegistrationStep'))
 // const PageNavigationActions = lazy(() => import('../../../components/PageNavigationActions'))
 // const SolarForm = lazy(() => import('../../../components/Forms/BasicInfos/SolarForm'))
 // const BasicLifeSupportForm = lazy(() => import('../../../components/Forms/BasicInfos/BasicLifeSupportForm'))
-// const BillAndMessageForm = lazy(() => import('../../../components/Forms/BasicInfos/BillAndMessageForm'))
+const BillAndMessageForm = lazy(() => import('../../../components/Forms/BasicInfos/BillAndMessageForm'))
 // const ControllerPreferencesSelector = lazy(() => import('../../../components/Inputs/ControllerPreferencesSelector'))
 
 const BasicInfoPage2 = () => {
   // const { registrationData, updateLeadMutation, createSiteMutation, setRegistrationData  } = useRegistration()
   // On load page get data from context
-  const { handleSubmit } = useForm({ mode: 'all' })
+  const { handleSubmit, control } = useForm({ mode: 'all' })
   // const navigate = useNavigate()
 
   // const [
@@ -77,10 +76,8 @@ const BasicInfoPage2 = () => {
     <form onSubmit={handleSubmit(onSubmit)} className="">
       {/* <RegistrationStep currentStep={0} />
       <hr className="hidden lg:block" /> */}
-      <p>How would you like to receive your bills and other notices, like disconnection warnings and price change notifications?</p>
-      <Button>TEST</Button>
-      <Button>TEST</Button>
-      {/* <BillAndMessageForm control={control} /> */}
+
+      <BillAndMessageForm control={control} />
       {/* <BasicLifeSupportForm control={control} /> */}
       {/* <SolarForm control={control} solar={solar}/>
       <ControllerPreferencesSelector name={'preferenceList'} control={control} rules={REQUIRED_VALIDATION} />
