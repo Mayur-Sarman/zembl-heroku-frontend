ccde554be0443c0d61688cc327e856c7492da81f
JamesTeerawat <james@skie.com.au>
2023-11-17 03:06:56 +0700

    Revert "update remove content"
    This reverts commit 2c3b5bfb2ec1619b051322e9f39ed256459e145c.

--- src/pages/BasicInfoPage/BasicInfoPage2/BasicInfoPage2.tsx
+++ src/pages/BasicInfoPage/BasicInfoPage2/BasicInfoPage2.tsx
@@ -1,12 +1,12 @@
 import { FieldValues, useForm } from 'react-hook-form'
 
-// import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 // import { REQUIRED_VALIDATION } from '../../../constants/validation'
-// import { useRegistration } from '../../../hooks/useRegistration'
-// import { LEAD_STATUS_CONVERTED_WON } from '../../../constants'
-// import { Site } from '../../../api/site'
-// import { convertPreference } from '../../../api/common'
-// import { ZEMBL_DEBUG_MODE } from '../../../constants/misc'
+import { useRegistration } from '../../../hooks/useRegistration'
+import { LEAD_STATUS_CONVERTED_WON } from '../../../constants'
+import { Site } from '../../../api/site'
+import { convertPreference } from '../../../api/common'
+import { ZEMBL_DEBUG_MODE } from '../../../constants/misc'
 import { lazy } from 'react'
 
 // const RegistrationStep = lazy(() => import('../../../components/RegistrationStep'))
@@ -17,10 +17,10 @@ const BillAndMessageForm = lazy(() => import('../../../components/Forms/BasicInf
 // const ControllerPreferencesSelector = lazy(() => import('../../../components/Inputs/ControllerPreferencesSelector'))
 
 const BasicInfoPage2 = () => {
-  // const { registrationData, updateLeadMutation, createSiteMutation, setRegistrationData  } = useRegistration()
+  const { registrationData, updateLeadMutation, createSiteMutation, setRegistrationData  } = useRegistration()
   // On load page get data from context
-  const { handleSubmit, control } = useForm({ mode: 'all' })
-  // const navigate = useNavigate()
+  const { handleSubmit, control } = useForm({ defaultValues: registrationData as FieldValues, mode: 'all' })
+  const navigate = useNavigate()
 
   // const [
   //   solar,
@@ -28,48 +28,47 @@ const BasicInfoPage2 = () => {
   //   'solar',
   // ]) as string[]
 
-  const onSubmit = (data: FieldValues) => {
-    console.log(data)
+  const onSubmit = async (data: FieldValues) => {
     // Call API
-    // const lead = { id: (data?.leadId as string) ?? '', status: LEAD_STATUS_CONVERTED_WON }
+    const lead = { id: (data?.leadId as string) ?? '', status: LEAD_STATUS_CONVERTED_WON }
 
-    // try {
-    //   const leadConvertResult = await updateLeadMutation.mutateAsync(lead)
-    //   const leadId = leadConvertResult?.processLeadOutput?.id ?? null
+    try {
+      const leadConvertResult = await updateLeadMutation.mutateAsync(lead)
+      const leadId = leadConvertResult?.processLeadOutput?.id ?? null
 
-    //   const selectedPreferences: string[] = (data?.preferenceList as string[]) ?? []
-    //   const siteData: Site = {
-    //     leadId: leadId,
-    //     gas: !!data?.gas,
-    //     electricity: !!data?.electricity,
-    //     siteType: data?.recordType as string,
-    //     billType: data?.billType as string,
-    //     lifeSupport: data?.lifeSupport as string,
-    //     solar: data?.solar as string,
-    //     solarConsideration: data?.solarConsideration as string,
-    //     preferences: convertPreference(selectedPreferences),
-    //     ...registrationData?.address as Record<string, string>
-    //   }
-    //   const createSiteResult = await createSiteMutation.mutateAsync(siteData)
+      const selectedPreferences: string[] = (data?.preferenceList as string[]) ?? []
+      const siteData: Site = {
+        leadId: leadId,
+        gas: !!data?.gas,
+        electricity: !!data?.electricity,
+        siteType: data?.recordType as string,
+        billType: data?.billType as string,
+        lifeSupport: data?.lifeSupport as string,
+        solar: data?.solar as string,
+        solarConsideration: data?.solarConsideration as string,
+        preferences: convertPreference(selectedPreferences),
+        ...registrationData?.address as Record<string, string>
+      }
+      const createSiteResult = await createSiteMutation.mutateAsync(siteData)
 
-    //   setRegistrationData((value) => {
-    //     const mergedValue = {
-    //       ...value,
-    //       ...siteData,
-    //       ...createSiteResult?.processSiteOutput,
-    //       preferenceList: selectedPreferences,
-    //       leadId: leadId,
-    //     }
+      setRegistrationData((value) => {
+        const mergedValue = {
+          ...value,
+          ...siteData,
+          ...createSiteResult?.processSiteOutput,
+          preferenceList: selectedPreferences,
+          leadId: leadId,
+        }
 
-    //     return mergedValue
-    //   })
-    //   navigate('/bill-upload')
-    // } catch (error) {
-    //   if (ZEMBL_DEBUG_MODE) console.log('BASIC_INFO_2_SUBMIT_ERROR:', error)
-    // } finally {
-    //   updateLeadMutation.reset()
-    //   createSiteMutation.reset()
-    // }
+        return mergedValue
+      })
+      navigate('/bill-upload')
+    } catch (error) {
+      if (ZEMBL_DEBUG_MODE) console.log('BASIC_INFO_2_SUBMIT_ERROR:', error)
+    } finally {
+      updateLeadMutation.reset()
+      createSiteMutation.reset()
+    }
   }
 
   return (
