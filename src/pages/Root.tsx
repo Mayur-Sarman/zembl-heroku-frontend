7519f6720429dca22c13773e398187020b66d211
JamesTeerawat <james@skie.com.au>
2023-11-17 03:07:12 +0700

    Revert "[Update]"
    This reverts commit 4c2dbe9d2d3e34fa9eff8d299740260ad92b8566.

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom'
 
 import { AlertContextProvider } from '../contexts'
 import Header from '../components/Header'
-// import Footer from '../components/Footer'
+import Footer from '../components/Footer'
 import { RegistrationContextProvider } from '../contexts/RegistrationContext'
 
 function Root() {
