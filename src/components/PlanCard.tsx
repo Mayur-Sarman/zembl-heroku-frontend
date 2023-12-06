279b0a3ae909a95ff56b5245741c2fa32115483e
JamesTeerawat <james@skie.com.au>
2023-12-06 13:52:16 +0700

    [UPDATE] logo scaling

--- src/components/PlanCard.tsx
+++ src/components/PlanCard.tsx
@@ -33,8 +33,8 @@ const PlanCard = ({
     <Card className={`bg-zembl-s1 w-full border ${isSelected ? 'border-green-400 border-2' : ''}`}>
       <CardBody className={`flex flex-col ${planDescription ? 'gap-y-6' : ''}`}>
         <div className={`flex flex-wrap flex-shrink-0 lg:flex-nowrap gap-4 lg:gap-6`}>
-          <div className="w-36 relative">
-            {logoURL ? <img src={logoURL} alt="Brand Logo" className="w-36 h-auto absolute inset-x-0 inset-y-0 m-auto" /> : null}
+          <div className="w-[144px] relative">
+            {logoURL ? <img src={logoURL} alt="Brand Logo" className="w-[144px] h-auto absolute inset-x-0 inset-y-0 m-auto" /> : null}
             {logoImageHTML ? (
               <div
                 className="flex items-center justify-center  grow-0 h-full"
