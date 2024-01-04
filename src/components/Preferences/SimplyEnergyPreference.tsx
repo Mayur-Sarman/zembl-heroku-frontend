79fc8db952dea25c90494da8a30e218a5c0abfb8
JamesTeerawat <james@skie.com.au>
2024-01-04 19:37:40 +0700

    [Update] change wording

--- src/components/Preferences/SimplyEnergyPreference.tsx
+++ src/components/Preferences/SimplyEnergyPreference.tsx
@@ -13,7 +13,7 @@ const SimplyEnergyPreference = () => {
         {`You have selected you want to receive your bills and other notices via ${word}`}
       </Typography>
       <TextNote className='text-sm'>
-        {`You can change the method you receive your bills anytime by contacing customer service. By switching to receive your bills via ${word}, you will be charged a fee of $1.65 including GST.`}
+        {`You can change the method you receive your bills anytime by contacting customer service. By switching to receive your bills via ${word}, you will be charged a fee of $1.65 including GST.`}
       </TextNote>
     </AccordionCard>
   )
