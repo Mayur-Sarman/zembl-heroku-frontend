0606e84ee80875d6fda7ec16044b76d4d481b06b
JamesTeerawat <james@skie.com.au>
2024-01-04 19:21:29 +0700

    [Updated] Simply pref word change

--- src/components/Preferences/SimplyEnergyPreference.tsx
+++ src/components/Preferences/SimplyEnergyPreference.tsx
@@ -1,8 +1,5 @@
-// import { Control } from 'react-hook-form'
 import AccordionCard from '../AccordionCard'
 import { useRegistration } from '../../hooks/useRegistration'
-// import ControllerSelectInput from '../Inputs/ControllerSelectInput'
-// import { SIMPLY_ENERGY_OPTIONS } from '../../constants'
 import TextNote from '../TextNote'
 import { Typography } from '@material-tailwind/react'
 
@@ -12,31 +9,14 @@ const SimplyEnergyPreference = () => {
   return (
 
     <AccordionCard alwaysOpen open title="Simply Energy Preferences" bodyClassName="flex-col text-left gap-y-6">
-
-      {/* <ControllerSelectInput
-          control={control}
-          name={`${prefix}.greenPowerOption`}
-          label={`You have selected you want to receive your bills and other notices via ${word}`}
-          textLabel={`You have selected you want to receive your bills and other notices via ${word}`}
-          placeholder="Select..."
-          options={SIMPLY_ENERGY_OPTIONS}
-          required
-        /> */}
       <Typography className='text-sm'>
         {`You have selected you want to receive your bills and other notices via ${word}`}
       </Typography>
       <TextNote className='text-sm'>
-        {`By receiving your bills via ${word} you will be charged a fee of $1.65 including GST. You can change the method you receive your bills anytime by contacting customer service.`}
+        {`You can change the method you receive your bills anytime by contacing customer service. By switching to receive your bills via ${word}, you will be charged a fee of $1.65 including GST.`}
       </TextNote>
     </AccordionCard>
   )
 }
-
-// interface SimplyEnergyPreferenceProps {
-//   control: Control
-//   siteAddress?: string
-//   prefix: string
-//   pref?: Record<string, string>
-// }
 
 export default SimplyEnergyPreference
